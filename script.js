3d554482b64f12fe8ce379e492de60aeebceda09
Matheus Souza <matheusgfs@outlook.com>
2025-07-17 19:51:30 -0300

    Arquivo de Estudos

---
+++ script.js
@@ -0,0 +1,48 @@
+const readline = require ("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const barbeiros =[];
+
+function perguntar(){
+    rl.question("Digite o nome do profissional para adicionar na lista: ", (resposta) => {
+        if (resposta.toLocaleLowerCase() ==="sair"){
+            console.log("\n Sua lista final:");
+            console.log(barbeiros);
+            rl.close();
+        }
+        else {
+            barbeiros.push(resposta);
+            console.log(`"Adicionado: "${resposta}"`);
+            perguntar();
+        }
+    });
+}
+
+console.log("Olá! Vamos adicionar os barbeiros");
+perguntar();
+
+
+
+/*const nome = 'Calvo';
+const idade = 32;
+
+console.log(`Seu nome é ${nome} e você tem ${idade} anos!`);
+
+var n1 = 5;
+var n2 = 3;
+var n3 = 4;
+n1+n2;
+console.log(n1+n2);
+
+var profissionais = ["Matheus", "Vitor", "Erick"];
+console.log(profissionais);
+
+var obj = {
+    nome: "Matheus",
+    idade: 32,
+};
+
+console.log(obj);*/
